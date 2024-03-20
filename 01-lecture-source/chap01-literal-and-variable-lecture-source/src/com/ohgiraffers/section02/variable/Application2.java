669ca909cf9262140a3964e68982bc01cdf2be20
DESKTOP-F4BC13L\hi <fodnjs2525@gamil.com>
2024-03-20 15:51:38 +0900

    변수 시작

---
+++ 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section02/variable/Application2.java
@@ -0,0 +1,33 @@
+package com.ohgiraffers.section02.variable;
+
+public class Application2 {
+
+    public static void mail(String[] args){
+        System.out.println();
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
